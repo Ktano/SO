a5c01b37184078b356b1b824588dae23ce0d43f7
Ktano <pedrocaetano56@gmail.com>
2016-10-23 22:58:28 +0100

    adicionadas outras funcoes

--- code/i-banco.c
+++ code/i-banco.c
@@ -70,14 +70,8 @@ int main (int argc, char** argv) {
     char *args[MAXARGS + 1];
     char buffer[BUFFER_SIZE];
     int nProcessos=0; /* guarda o numero de processos já criados*/
-	
-
-	
-	pthread_mutex_init(&bufferReadMutex,NULL);
-	sem_init(&semLeitura,0,0);
-	sem_init(&semEscrita,0,CMD_BUFFER_DIM);
     
-    inicializarContas();
+	inicializarContas();
 	inicializarTarefas();
 	sinalRecebido=0;
 	
@@ -86,7 +80,6 @@ int main (int argc, char** argv) {
 		exit(EXIT_SUCCESS);
 		}
 		
-	
     printf("Bem-vinda/o ao i-banco\n\n");
       
     while (1) {
@@ -108,6 +101,11 @@ int main (int argc, char** argv) {
 				
                 printf("i-banco vai terminar.\n");
                 printf("--\n");
+				
+				for(i=0;i<NUM_TRABALHADORAS;i++){
+					adicionarCommando(COMANDO_SAIR_ID,0,0);
+					pthread_join()
+				}
 				
                 for(i=0;i<nProcessos;i++){
 					pid=wait(&status);
@@ -161,27 +159,20 @@ int main (int argc, char** argv) {
 
         idConta = atoi(args[1]);
         valor = atoi(args[2]);
+		adicionarCommando(COMANDO_CREDITAR_ID,idConta,valor);
 
-        if (creditar (idConta, valor) < 0)
-            printf("%s(%d, %d): Erro\n\n", COMANDO_CREDITAR, idConta, valor);
-        else
-            printf("%s(%d, %d): OK\n\n", COMANDO_CREDITAR, idConta, valor);
     }
 
     /* Ler Saldo */
     else if (strcmp(args[0], COMANDO_LER_SALDO) == 0) {
-        int idConta, saldo;
+        int idConta;
 
         if (numargs < 2) {
             printf("%s: Sintaxe inválida, tente de novo.\n", COMANDO_LER_SALDO);
             continue;
         }
         idConta = atoi(args[1]);
-        saldo = lerSaldo (idConta);
-        if (saldo < 0)
-            printf("%s(%d): Erro.\n\n", COMANDO_LER_SALDO, idConta);
-        else
-            printf("%s(%d): O saldo da conta é %d.\n\n", COMANDO_LER_SALDO, idConta, saldo);
+		adicionarCommando(COMANDO_LER_SALDO_ID,idConta,0);
     }
 
     /* Simular */
@@ -233,6 +224,9 @@ void apanhaUSR1(int s){
 
 void inicializarTarefas(){
 	int i=0,pthread;
+	pthread_mutex_init(&bufferReadMutex,NULL);
+	sem_init(&semLeitura,0,0);
+	sem_init(&semEscrita,0,CMD_BUFFER_DIM);
 	pthread_t tid;
 	for(i=0;i<NUM_TRABALHADORAS;i++){
 		pthread=pthread_create(&tid,0,trabalhadora,NULL);
@@ -248,6 +242,7 @@ void *trabalhadora(){
 	while(1){
 		sem_wait(&semLeitura);
 		pthread_mutex_lock(&bufferReadMutex);
+		printf("working\n");
 		cmd=cmd_buffer[buff_read_idx];
 		buff_read_idx=(buff_read_idx+1)%CMD_BUFFER_DIM;
 		pthread_mutex_unlock(&bufferReadMutex);
@@ -259,8 +254,26 @@ void *trabalhadora(){
 			else
 				printf("%s(%d, %d): OK\n\n", COMANDO_DEBITAR, cmd.idConta, cmd.valor);
 		}
-	
-
+		
+		else if(cmd.operacao==COMANDO_CREDITAR_ID){
+			if (creditar (cmd.idConta, cmd.valor) < 0)
+				printf("%s(%d, %d): Erro\n\n", COMANDO_CREDITAR, cmd.idConta, cmd.valor);
+			else
+				printf("%s(%d, %d): OK\n\n", COMANDO_CREDITAR, cmd.idConta, cmd.valor);
+		}
+		else if(cmd.operacao==COMANDO_LER_SALDO_ID){
+			int saldo;
+			saldo = lerSaldo (cmd.idConta);
+			
+			if (saldo < 0)
+				printf("%s(%d): Erro.\n\n", COMANDO_LER_SALDO, cmd.idConta);
+			else
+				printf("%s(%d): O saldo da conta é %d.\n\n", COMANDO_LER_SALDO, cmd.idConta, saldo);
+		}
+		else if(cmd.operacao==COMANDO_SAIR_ID){
+			printf("A thread vai terminar");
+			pthread_exit(0);
+		}
 	}
 	return NULL;
 }
