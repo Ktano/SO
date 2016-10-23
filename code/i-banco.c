278ac03d1af4c8ad645cbf362f83798e657ef6a3
Ktano <Ktano@users.noreply.github.com>
2016-10-23 22:25:49 +0100

    inclui tarefas

--- code/i-banco.c
+++ code/i-banco.c
@@ -9,6 +9,8 @@
 // Ricardo Almeida, N:63108
 */
 
+/*array dois semaforos um para ler e outro semaforo tarefas com 0*/
+
 #include "commandlinereader.h"
 #include "contas.h"
 #include <stdlib.h>
@@ -19,6 +21,8 @@
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <pthread.h>
+#include <semaphore.h>
 
 
 
@@ -29,14 +33,36 @@
 #define COMANDO_SAIR "sair"
 #define ARGUMENTO_AGORA "agora"
 
+#define COMANDO_DEBITAR_ID 1
+#define COMANDO_CREDITAR_ID 2
+#define COMANDO_LER_SALDO_ID 3
+#define COMANDO_SIMULAR_ID 4
+#define COMANDO_SAIR_ID 5
+
 #define MAXARGS 3
 #define BUFFER_SIZE 100
 
+#define NUM_TRABALHADORAS 3
+#define CMD_BUFFER_DIM (NUM_TRABALHADORAS * 2)
+
+typedef struct{
+	int operacao;
+	int idConta;
+	int valor;
+} comando_t;
+
+comando_t cmd_buffer[CMD_BUFFER_DIM];
+int buff_write_idx = 0, buff_read_idx = 0;
+sem_t semLeitura,semEscrita;
+pthread_mutex_t bufferReadMutex;
 
 /*
 // Prototipos
 */
 void apanhaUSR1(int s);
+void inicializarTarefas();
+void *trabalhadora();
+void adicionarCommando(int Commando, int idConta, int valor);
 
 
 int main (int argc, char** argv) {
@@ -44,8 +70,15 @@ int main (int argc, char** argv) {
     char *args[MAXARGS + 1];
     char buffer[BUFFER_SIZE];
     int nProcessos=0; /* guarda o numero de processos já criados*/
+	
+
+	
+	pthread_mutex_init(&bufferReadMutex,NULL);
+	sem_init(&semLeitura,0,0);
+	sem_init(&semEscrita,0,CMD_BUFFER_DIM);
     
     inicializarContas();
+	inicializarTarefas();
 	sinalRecebido=0;
 	
 	if(signal(SIGUSR1,apanhaUSR1)==SIG_ERR){
@@ -113,11 +146,8 @@ int main (int argc, char** argv) {
 
             idConta = atoi(args[1]);
             valor = atoi(args[2]);
+			adicionarCommando(COMANDO_DEBITAR_ID,idConta,valor);
 
-            if (debitar (idConta, valor) < 0)
-	           printf("%s(%d, %d): Erro\n\n", COMANDO_DEBITAR, idConta, valor);
-            else
-                printf("%s(%d, %d): OK\n\n", COMANDO_DEBITAR, idConta, valor);
     }
 
     /* Creditar */
@@ -199,5 +229,47 @@ void apanhaUSR1(int s){
 			exit(EXIT_SUCCESS);
 		}
 	sinalRecebido=1;
-	}
+}
 
+void inicializarTarefas(){
+	int i=0,pthread;
+	pthread_t tid;
+	for(i=0;i<NUM_TRABALHADORAS;i++){
+		pthread=pthread_create(&tid,0,trabalhadora,NULL);
+		if(pthread!=0){
+			fprintf(stderr,"Erro ao criar thread");
+			continue;
+		}
+	}
+}
+	
+void *trabalhadora(){
+	comando_t cmd;
+	while(1){
+		sem_wait(&semLeitura);
+		pthread_mutex_lock(&bufferReadMutex);
+		cmd=cmd_buffer[buff_read_idx];
+		buff_read_idx=(buff_read_idx+1)%CMD_BUFFER_DIM;
+		pthread_mutex_unlock(&bufferReadMutex);
+		sem_post(&semEscrita);
+		
+		if(cmd.operacao==COMANDO_DEBITAR_ID){
+			if (debitar (cmd.idConta, cmd.valor) < 0)
+				printf("%s(%d, %d): Erro\n\n", COMANDO_DEBITAR, cmd.idConta, cmd.valor);
+			else
+				printf("%s(%d, %d): OK\n\n", COMANDO_DEBITAR, cmd.idConta, cmd.valor);
+		}
+	
+
+	}
+	return NULL;
+}
+
+void adicionarCommando(int Commando, int idConta, int valor){
+	sem_wait(&semEscrita);
+	cmd_buffer[buff_write_idx].operacao=(Commando);
+	cmd_buffer[buff_write_idx].idConta=(idConta);
+	cmd_buffer[buff_write_idx].valor=(valor);
+	buff_write_idx=(buff_write_idx+1)%CMD_BUFFER_DIM;
+	sem_post(&semLeitura);
+}
